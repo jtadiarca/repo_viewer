13f5cbfe29cefc97cb1dd3cf76b0b2f4c3e6fa31
Joseph Tadiarca <avegailtadiarca@gmail.com>
2021-06-06 20:08:08 -0500

    Secure Credentials Storage

--- lib/auth/infrastructure/credentials_storage/credentials_storage.dart
+++ lib/auth/infrastructure/credentials_storage/credentials_storage.dart
@@ -3,7 +3,7 @@ import 'package:oauth2/oauth2.dart';
 abstract class CredentialsStorage {
   Future<Credentials?> read();
 
-  Future<void> save(Credentials);
+  Future<void> save(Credentials credentials);
 
   Future<void> clear();
 }
