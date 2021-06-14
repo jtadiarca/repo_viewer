12824ece675f24bc99244c76138a28d2335dd3c9
Joseph Tadiarca <avegailtadiarca@gmail.com>
2021-06-13 21:20:24 -0500

    Start Auth Application Layer

---
+++ lib/auth/application/auth_notifier.dart
@@ -0,0 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:repo_viewer/auth/domain/auth_failure.dart';
+
+part 'auth_notifier.freezed.dart';
+
+@freezed
+class AuthState with _$AuthState {
+  const AuthState._();
+  const factory AuthState.initial() = _Initial;
+  const factory AuthState.unauthenticated() = _Unauthenticated;
+  const factory AuthState.authenticated() = _Authenticated;
+  const factory AuthState.failure(AuthFailure failure) = _Failure;
+}
--- lib/auth/infrastructure/github_authenticator.dart
+++ lib/auth/infrastructure/github_authenticator.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/services.dart';
