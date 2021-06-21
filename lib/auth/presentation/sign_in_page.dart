76a5e8c5eca3aa9f8516509121b27680e7895b0a
Joseph Tadiarca <avegailtadiarca@gmail.com>
2021-06-20 23:13:47 -0500

    Navigating Based on the AuthState

--- lib/auth/presentation/sign_in_page.dart
+++ lib/auth/presentation/sign_in_page.dart
@@ -5,6 +5,6 @@ class SignInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold();
   }
 }
--- lib/core/presentation/app_widget.dart
+++ lib/core/presentation/app_widget.dart
@@ -2,6 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../auth/application/auth_notifier.dart';
 import '../../auth/shared/providers.dart';
 import 'routes/app_router.gr.dart';
 
@@ -12,18 +13,41 @@ final initializationProvider = FutureProvider<Unit>((ref) async {
   return unit;
 });
 
-class AppWidget extends StatelessWidget {
+class AppWidget extends ConsumerWidget {
   final appRouter = AppRouter();
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetReference ref) {
+    /* ref.listen<AuthState>(authNotifier (state) {
+
+    }); */
+
     return ProviderListener(
       provider: initializationProvider,
       onChange: (context, value) {},
-      child: MaterialApp.router(
-        title: 'Repo Viewer',
-        routerDelegate: appRouter.delegate(),
-        routeInformationParser: appRouter.defaultRouteParser(),
+      child: ProviderListener<AuthState>(
+        provider: authNotifierProvider,
+        onChange: (context, state) {
+          state.maybeMap(
+              orElse: () {},
+              authenticated: (_) {
+                appRouter.pushAndPopUntil(
+                  const StarredReposRoute(),
+                  predicate: (route) => false,
+                );
+              },
+              unauthenticated: (_) {
+                appRouter.pushAndPopUntil(
+                  const SignInRoute(),
+                  predicate: (route) => false,
+                );
+              });
+        },
+        child: MaterialApp.router(
+          title: 'Repo Viewer',
+          routerDelegate: appRouter.delegate(),
+          routeInformationParser: appRouter.defaultRouteParser(),
+        ),
       ),
     );
   }
