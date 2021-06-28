ad0ce04cdca5299ff22e79b0716dcfc4bc55b2fe
Joseph Tadiarca <avegailtadiarca@gmail.com>
2021-06-28 15:54:21 -0500

    Android-specific WebView

--- lib/auth/presentation/authorization_page.dart
+++ lib/auth/presentation/authorization_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:repo_viewer/auth/infrastructure/github_authenticator.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -17,6 +19,14 @@ class AuthorizationPage extends StatefulWidget {
 }
 
 class _AuthorizationPageState extends State<AuthorizationPage> {
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) {
+      WebView.platform = SurfaceAndroidWebView();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
