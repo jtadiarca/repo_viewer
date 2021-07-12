2fd57bcc7e819848ebb4cc398a9f139359030b9f
Joseph Tadiarca <avegailtadiarca@gmail.com>
2021-07-11 19:29:08 -0500

    Github Failure

---
+++ lib/github/core/domain/github_failure.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'github_failure.freezed.dart';
+
+@freezed
+class GithubFailure with _$GithubFailure {
+  const GithubFailure._();
+  const factory GithubFailure.api(HttpStatus httpStatus) = _Api;
+}
