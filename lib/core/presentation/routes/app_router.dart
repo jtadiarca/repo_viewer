6b902bf2a97052e03d3e271a91bfa66d9990fe73
Joseph Tadiarca <avegailtadiarca@gmail.com>
2021-07-11 19:07:56 -0500

    Creating the Github Feature Folder structure

--- lib/core/presentation/routes/app_router.dart
+++ lib/core/presentation/routes/app_router.dart
@@ -1,9 +1,9 @@
 import 'package:auto_route/annotations.dart';
-import 'package:repo_viewer/auth/presentation/authorization_page.dart';
-import 'package:repo_viewer/auth/presentation/sign_in_page.dart';
 
+import '../../../auth/presentation/authorization_page.dart';
+import '../../../auth/presentation/sign_in_page.dart';
+import '../../../github/repos/starred_repos/presentation/starred_repos_page.dart';
 import '../../../splash/presentation/splash_page.dart';
-import '../../../starred_repos/presentation/starred_repos_page.dart';
 
 @MaterialAutoRouter(
   routes: [
