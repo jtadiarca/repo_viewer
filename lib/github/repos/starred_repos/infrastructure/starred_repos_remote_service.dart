45aea018c2cd99349254c7bc529d9360d328436d
Joseph Tadiarca <avegailtadiarca@gmail.com>
2021-07-22 17:36:38 -0500

    Caching the GitHub Headers

--- lib/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart
+++ lib/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart
@@ -1,16 +1,21 @@
 import 'package:dio/dio.dart';
 
 import '../../../../core/infrastructure/remote_response.dart';
+import '../../../core/infrastructure/github_headers_cache.dart';
 import '../../../core/infrastructure/github_repo_dto.dart';
 
 class StarredReposRemoteService {
   final Dio _dio;
+  final GithubHeadersCache _headersCache;
 
-  StarredReposRemoteService(this._dio);
+  StarredReposRemoteService(
+    this._dio,
+    this._headersCache,
+  );
 
-  // Future<RemoteResponse<List<GithubRepoDto>>> getStarredReposPage(
-  //   int page,
-  // ) async {
-  //   // _dio.get(path)
-  // }
+  Future<RemoteResponse<List<GithubRepoDto>>> getStarredReposPage(
+    int page,
+  ) async {
+    _dio.get('');
+  }
 }
