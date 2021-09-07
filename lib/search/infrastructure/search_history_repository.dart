78f210f9c51a6443a0daacafb2ac9816d29b44bb
Joseph Tadiarca <avegailtadiarca@gmail.com>
2021-09-06 19:28:13 -0500

    Watching Search Terms

---
+++ lib/search/infrastructure/search_history_repository.dart
@@ -0,0 +1,26 @@
+import 'package:sembast/sembast.dart';
+
+import '../../core/infrastructure/sembast_database.dart';
+
+class SearchHistoryRepository {
+  final SembastDatabase _sembastDatabase;
+  final _store = StoreRef<int, String>('searchHistory');
+
+  SearchHistoryRepository(this._sembastDatabase);
+
+  static const historyLength = 10;
+
+  Stream<List<String>> watchSearchTerms({String? filter}) {
+    return _store
+        .query(
+          finder: filter != null && filter.isNotEmpty
+              ? Finder(
+                  filter: Filter.custom(
+                    (record) => (record.value as String).startsWith(filter),
+                  ),
+                )
+              : null,
+        )
+        .onSnapshots(_sembastDatabase.instance);
+  }
+}
