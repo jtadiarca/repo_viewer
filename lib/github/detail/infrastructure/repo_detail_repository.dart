ca7288404972df351ef82bb5fb16f82d1f60b866
Joseph Tadiarca <avegailtadiarca@gmail.com>
2021-11-01 14:34:41 -0500

    Repo Detail Repository Part 2

--- lib/github/detail/infrastructure/repo_detail_repository.dart
+++ lib/github/detail/infrastructure/repo_detail_repository.dart
@@ -1,7 +1,9 @@
 import 'package:dartz/dartz.dart';
+import 'package:repo_viewer/core/domain/fresh.dart';
 import 'package:repo_viewer/core/infrastructure/network_exceptions.dart';
 import 'package:repo_viewer/github/core/domain/github_failure.dart';
 import 'package:repo_viewer/github/detail/domain/github_repo_detail.dart';
+import 'package:repo_viewer/github/detail/infrastructure/github_repo_detail_dto.dart';
 import 'package:repo_viewer/github/detail/infrastructure/repo_detail_local_service.dart';
 import 'package:repo_viewer/github/detail/infrastructure/repo_detail_remote_service.dart';
 
@@ -10,6 +12,45 @@ class RepoDetailRepository {
   final RepoDetailRemoteService _remoteService;
 
   RepoDetailRepository(this._localService, this._remoteService);
+
+  Future<Either<GithubFailure, Fresh<GithubRepoDetail?>>> getRepoDetail(
+    String fullRepoName,
+  ) async {
+    try {
+      final htmlRemoteResponse =
+          await _remoteService.getReadmeHtml(fullRepoName);
+
+      return right(
+        await htmlRemoteResponse.when(
+          noConnection: () async => Fresh.no(
+            await _localService
+                .getRepoDetail(fullRepoName)
+                .then((dto) => dto?.toDomain()),
+          ),
+          notModified: (_) async {
+            final cached = await _localService.getRepoDetail(fullRepoName);
+            final starred = await _remoteService.getStarredStatus(fullRepoName);
+            final withUpdatedStarredField =
+                cached?.copyWith(starred: starred ?? false);
+
+            return Fresh.yes(withUpdatedStarredField?.toDomain());
+          },
+          withNewData: (html, _) async {
+            final starred = await _remoteService.getStarredStatus(fullRepoName);
+            final dto = GithubRepoDetailDto(
+              fullName: fullRepoName,
+              html: html,
+              starred: starred ?? false,
+            );
+            await _localService.upSertRepoDetail(dto);
+            return Fresh.yes(dto.toDomain());
+          },
+        ),
+      );
+    } on RestApiException catch (e) {
+      return left(GithubFailure.api(e.errorCode));
+    }
+  }
 
   /// Returns `right(null)` if there's no Internet connection
   Future<Either<GithubFailure, Unit?>> switchStarredStatus(
